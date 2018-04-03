f13c8b54a37d18b7a42de1267945f9f310c7c19a
Taesu Hyeon <qvil1127@gmail.com>
2018-04-03 19:21:08 +0900

    Apply hello saga

--- redux-saga/counter/src/index.js
+++ redux-saga/counter/src/index.js
@@ -2,18 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
 
 import Counter from "./Counter";
 import reducer, { INCREMENT, DECREMENT } from "./reducer";
+import { helloSaga } from "./sagas";
 
+const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers =
   process.env.NODE_ENV !== "production"
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
     : compose;
-
-const store = createStore(reducer, composeEnhancers(applyMiddleware()));
-
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
 const action = type => store.dispatch({ type });
+
+sagaMiddleware.run(helloSaga);
 
 function render() {
   ReactDOM.render(
